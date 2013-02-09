24a760ad50eb4da64df975ee7efa73782783c684
Jonathan Saggau <saggau@gmail.com>
2013-02-08 21:00:04 -0500

    added Custom Segue for if the class starts chp at the beginning

---
+++ CustomSegueFromBeginning/CustomSegue/BNRViewController.h
@@ -0,0 +1,15 @@
+//
+//  BNRViewController.h
+//  CustomSegue
+//
+//  Created by Brandon Newendorp on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIButton *segueButton;
+
+@end
---
+++ CustomSegueFromBeginning/CustomSegue/BNRViewController.m
@@ -0,0 +1,37 @@
+//
+//  BNRViewController.m
+//  CustomSegue
+//
+//  Created by Brandon Newendorp on 1/26/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BNRViewController.h"
+
+@implementation BNRViewController
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+	    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+	} else {
+	    return YES;
+	}
+}
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue
+                sender:(id)sender
+{
+    // Set title of navigation bar to destination view controller title
+    // (not sure why this isn't happening automatically)
+    UIViewController *dest = segue.destinationViewController;
+    dest.navigationItem.title = dest.title;
+}
+
+@end
